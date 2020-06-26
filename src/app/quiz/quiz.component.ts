66cc1528eac1dec53d7eca38f30b5606cc983674
Wesklin <weskley.s.r@hotmail.com>
2020-06-26 17:20:55 -0300

    Detalhes Quiz e Ranking
    Ajutes nos camos e pontos nas escolhas corretas

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -14,7 +14,8 @@ export class QuizComponent implements OnInit {
 
     nomesRef: AngularFireList<any>;
     nomes: Observable<any[]>;
-  
+
+    contador: number =0;
     constructor(private db: AngularFireDatabase) {
       this.listRef = db.list('questions');
       this.list = this.listRef.snapshotChanges().pipe(
@@ -29,14 +30,30 @@ export class QuizComponent implements OnInit {
         )
       );
     }
-  
-      add(nm: string) {
+
+    correto(r: any, verdadeiro: any){
+      console.log(r + " e " + verdadeiro)
+      if (r == verdadeiro){
+        this.contador++
+      } else if (this.contador == 0){
+        this.contador==0
+      }else{
+        this.contador--
+      }
+      console.log(this.contador)
+    }
+
+      add(nm: string, acerto: number) {
+        if(nm == ""){
+
+        }else{
       this.nomesRef.push(
         {
           nome: nm,
+          acertos: acerto
         }
       );
-    }
+    }    }
 
   
   
--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -2,27 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { element } from 'protractor';
+import { async } from '@angular/core/testing';
+
+export interface Pessoa {
+  nome: string;
+  acertos: number;
+}
 @Component({
   selector: 'app-ranking',
   templateUrl: './ranking.component.html',
   styleUrls: ['./ranking.component.css']
 })
+
+
 export class RankingComponent implements OnInit {
 
   listRef: AngularFireList<any>;
   list: Observable<any[]>;
 
-  constructor(private db: AngularFireDatabase) {
+   constructor(private db: AngularFireDatabase) {
       this.listRef = db.list('nomes');
       this.list = this.listRef.snapshotChanges().pipe(
-        map(changes =>
-          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+        map(changes => 
+          changes.map(
+            c => ({ 
+            key: c.payload.key, ...c.payload.val() }))
         )
-      );
-  }
+           );
 
-
-
+          }
   ngOnInit() {
   }
 
